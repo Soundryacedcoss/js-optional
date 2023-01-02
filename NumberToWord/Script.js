9e95014db61a4ebbc0a00cc9fcdbc10b2c5ffad8
Soundryacedcoss <soundryarani@cedcoss.com>
2023-01-02 16:04:23 +0530

    Done

--- NumberToWord/Script.js
+++ NumberToWord/Script.js
@@ -1,35 +1,28 @@
-var th = [
+let oneToTy = [
   "",
-  "thousand",
-  "million",
-  "billion",
-  "trillion"
+  "one ",
+  "two ",
+  "three ",
+  "four ",
+  "five ",
+  "six ",
+  "seven ",
+  "eight ",
+  "nine ",
+  "ten ",
+  "eleven ",
+  "twelve ",
+  "thirteen ",
+  "fourteen ",
+  "fifteen ",
+  "sixteen ",
+  "seventeen ",
+  "eighteen ",
+  "nineteen ",
 ];
-var dg = [
-  "zero",
-  "one",
-  "two",
-  "three",
-  "four",
-  "five",
-  "six",
-  "seven",
-  "eight",
-  "nine",
-];
-var tn = [
-  "ten",
-  "eleven",
-  "twelve",
-  "thirteen",
-  "fourteen",
-  "fifteen",
-  "sixteen",
-  "seventeen",
-  "eighteen",
-  "nineteen",
-];
-var tw = [
+let tenth = [
+  "",
+  "",
   "twenty",
   "thirty",
   "forty",
@@ -39,46 +32,43 @@ var tw = [
   "eighty",
   "ninety",
 ];
-
 function toWords() {
-  var s = document.getElementById("input").value;
-  s = s.replace(/[\, ]/g, "");
-  var x = s.indexOf(".");
-  console.log(x);
-  if (x == -1) x = s.length;
-  console.log(x);
-  if (x > 15) document.getElementById("Output").innerHTML = "too big";
-  var n = s.split("");
-  var str = "";
-  var sk = 0;
-  console.log(x);
-  for (var i = 0; i < x; i++) {
-    console.log(dg[n[i]]);
-    if ((x - i) % 3 == 2) {
-      if (n[i] == "1") {
-        str += tn[Number(n[i + 1])] + " ";
-        i++;
-        sk = 1;
-      } else if (n[i] != 0) {
-        str += tw[n[i] - 2] + " ";
-        sk = 1;
-      }
-    } else if (n[i] != 0) {
-      str += dg[n[i]] + " ";
-      if ((x - i) % 3 == 0) str += "hundred ";
-      sk = 1;
-    }
-    if ((x - i) % 3 == 1) {
-      if (sk) str += th[(x - i - 1) / 3] + " ";
-      sk = 0;
-    }
-  }
+  var input = document.getElementById("input").value;
+  if (input.toString().length > 7) {
+    alert("Too big");
+    document.getElementById("Output").innerHTML = " ";
+  } else {
+    let num = ("0000000" + input)
+      .slice(-7)
+      .match(/^(\d{1})(\d{1})(\d{2})(\d{1})(\d{2})$/);
+    console.log(num);
+    if (!num) return;
+    let outputText =
+      num[1] != 0
+        ? (oneToTy[Number(num[1])] ||
+            `${tenth[num[1][0]]} ${oneToTy[num[1][1]]}`) + " million "
+        : "";
 
-  if (x != s.length) {
-    var y = s.length;
-    str += "point ";
-    for (var i = x + 1; i < y; i++) str += dg[n[i]] + " ";
+    outputText +=
+      num[2] != 0
+        ? (oneToTy[Number(num[2])] ||
+            `${tenth[num[2][0]]} ${oneToTy[num[2][1]]}`) + "hundred "
+        : "";
+    outputText +=
+      num[3] != 0
+        ? (oneToTy[Number(num[3])] ||
+            `${tenth[num[3][0]]} ${oneToTy[num[3][1]]}`) + " thousand "
+        : "";
+    outputText +=
+      num[4] != 0
+        ? (oneToTy[Number(num[4])] ||
+            `${tenth[num[4][0]]} ${oneToTy[num[4][1]]}`) + "hundred "
+        : "";
+    outputText +=
+      num[5] != 0
+        ? oneToTy[Number(num[5])] ||
+          `${tenth[num[5][0]]} ${oneToTy[num[5][1]]} `
+        : "";
+    document.getElementById("Output").innerHTML = outputText;
   }
-  document.getElementById("Output").innerHTML=str
-  return str.replace(/\s+/g, " ");
 }
